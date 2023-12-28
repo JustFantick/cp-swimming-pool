570e64a5c8eab11954a571487f372f157b4a50e1
JustFantick <68466135+JustFantick@users.noreply.github.com>
2023-12-28 16:44:30 +0200

    feat: edit CustomForm component
    Add auto form reset after sending request to the server

--- src/components/forms/CustomForm.jsx
+++ src/components/forms/CustomForm.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+'use client'
+import React, { useRef } from 'react'
 
 const CustomForm = ({ children, action }) => {
+	const formRef = useRef(null);
+
 	return (
-		<form action={action}>
+		<form ref={formRef} action={async formData => {
+			formRef.current?.reset();
+			await action(formData);
+		}}>
 			{children}
 		</form>
 	)
